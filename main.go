875f023de10fd66b2c6ad1fc6a2ab60712671133
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-02 09:23:57 +0400

    add Photo, PhotoSource, SearchResult types

--- main.go
+++ main.go
@@ -15,9 +15,39 @@ type Client struct{
 	RemainingTimes int32
 }
 
+type Photo struct{
+	ID								int32				`json:"id"`
+	Width							int32				`json:"width"`
+	Height						int32				`json:"height"`
+	URL								string			`json:"url"`
+	Photographer			string			`json:"photographer"`
+	PhotographerURL		string			`json:"photographer_url"`
+	Src								PhotoSource	`json:"json:"src"`
+}
+
+type PhotoSource struct{
+	Original		string		`json:"original"`
+	Large				string		`json:"large"`
+	Large2x			string		`json:"large2x"`
+	Medium			string		`json:"medium"`
+	Small				string		`json:"small"`
+	Portrait		string		`json:"portrait"`
+	Square			string		`json:"square"`
+	Landscape		string		`json:"landscape"`
+	Tiny				string		`json:"tiny"`
+}
+
 func NewClient(token string) *Client{
 	c := http.Client()
 	return &Client{Token: token, hc: c}
+}
+
+type SearchResult struct{
+	Page 					int32	`json:"page"`
+	PerPage 			int32  `json:"per_page"`
+	TotalResults	int32		`json:"total_results"`
+	NextPage 			string	`json:"next_page"`
+	Photos 				[]Photo	`json:"photos"`
 }
 
 const(
