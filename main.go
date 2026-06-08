2fc8a72958c2ae3761a7dc34e12afea19acb8702
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-08 14:29:36 +0400

    add GetRandomPhoto using CuratedPhotos with random page

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import(
 	"encoding/json"
 	"strconv"
 	"time"
+	"math/rand"
 	"io"
 )
 
@@ -137,6 +138,16 @@ func (c *Client) GetPhoto(id int32)(*Photo, error){
 	var result Photo
 	err = json.Unmarshal(data, &result)
 	return &result, err
+}
+
+func(c *Client) GetRandomPhoto()(*Photo, error){
+	rand.Seed(time.Now().Unix())
+	randNum := rand.Intn(1001)
+	result, err := c.CuratedPhotos(1, randNum)
+	if err == nil && len(result.Photos)==1{
+		return &result.Photos[0], nil
+	}
+	return nil, err
 }
 
 
